7036b1db583e70ff41c63b9c417e1ee5333db9b4
Tushiar <tusharmoolya1234@gmail.com>
2022-12-07 00:28:58 +0530

    commented unused code

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(cors())
 app.use('/api/auth', require('./routes/api/auth'));
 
 //Serve static assets in production
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'))
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static('client/build'))
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+//   })
+// }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(process.env.MONGO_URI)
